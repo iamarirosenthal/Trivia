5db1e5957e60e18def249b08fa746d91ef9c22d1
iamarirosenthal <iamarirosenthal@outlook.com>
2017-09-20 12:55:59 -0400

    Finish

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,168 +1,249 @@
 $(document).ready(function() {
+    var index = 0;
+    var countdownTimer = {
+        time : 30,
+        reset: function() {
+            this.time = 30;
+            $('.timer').html('<h3>' + this.time + ' seconds remaining</h3>');
+        },
+        start: function() {
+            counter = setInterval(countdownTimer.count, 1000);  
+        },
+        stop: function() {
+            clearInterval(counter);
+        },
+        count: function() {
+                countdownTimer.time--;
+                console.log(countdownTimer.time);
 
-	//this section is coding for the timer
-	var index = 0; 
-	var countdownTimer = {
-		time : 30,
-		reset : function() {
-			this.time = 30; 
-			$('.timer').html('<h3>' + this.time + 'seconds remaining</h3>' ); 
-		},
-		start: function() {
-			count = setInterval(countdownTimer.count, 1000); 
-		}, 
-		  stop: function() {
-		  	clearInterval(counter); 
-	    },
-	     count: function() {
-	     	             countdownTimer.time--;
-	     	             console.log(countdownTimer.time);
-	     	          //   $('.timer').html(countdownTimer.time);
-	     	           if(countdownTimer.time >= 0) {
-	     	           		$('.timer').html('<h3>' + countdownTimer + 'seconds remaining</h3>');
+            if (countdownTimer.time >= 0) {
+                $('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
+            }
+            else {
+                index++;
+                answerWrong();
+                countdownTimer.reset();
+                if (index < questionArray.length) {
+                    loadQuestion(index);
+                } else {
+                    $(".answerchoice").hide();
+                    showScore();
+                }
+            }
+        }
+    };
 
-	     	           }
-	     	           else {
+var correct = 0;
+var wrong = 0;
+var q1 = {
+    question : 'WWhat was the license number on the Ghostbusters car?',
+    possibleAnswers : ['A. TCIP-2',
+                 'B. NCIS-3',
+                 'C. ECTO-1',
+                 'D. T910-5'],
+    flags : [false, false, true, false],
+    answer : 'C. ECTO-1'
+};
 
-	     	           		index++;
-	     	           		answerWrong();
-	     	           		countdownTimer.reset();
-	     	           		if (index < questionArray.length) {
-	     	           					loadQuestion(index); 
+var q2 = {
+    question: 'What is the name of the Freelings dog in Poltergeist',
+    possibleAnswers: ['A. Spazz',
+                 'B. Ebuzz',
+                 'C. Carpe',
+                 'D. Chance'],
+    flags : [false, true, false, false],
+    answer : 'Ebuzz'
+};
 
-	     	           		}else {
+var q3 = {
+    question : 'The main street in Back to the Future is also the main street in what other 80s movie?',
+    possibleAnswers : ['A. Pretty In Pink',
+                 'B. Gremlins',
+                 'C. St. Elmos Fire',
+                 'D. My Cousin Vinny'],
+    flags : [false, true, false, false],
+    answer : 'B. Gremlins'
+};
 
-	     	           			  $(".answerChoice").hide(); 
-	     	           			  showScore(); 
-	     	           		}
+var q4 = {
+    question : 'In "The Karate Kid" what color did Daniel have to paint Miagis house, as part of his training?',
+    possibleAnswers : ['A. Green',
+                 'B. Yellow',
+                 'C. Brown',
+                 'D. Red'],
+    flags : [true, false, false, false],
+    answer : 'A. Green'
+};
 
-	     	           }
-	          
-	           }
+var q5 = {
+    question : 'What is Indiana Jones main weapon?',
+    possibleAnswers : ['A. Gun',
+                 'B. Whip',
+                 'C. Dagger',
+                 'D. Sword'],
+    flags : [false, true, false, false],
+    answer : 'B. Whip'
+};
 
-	      }
+var q6 = {
+    question : 'What is the occupation of Tom Hank character in Bachelor Party?',
+    possibleAnswers : ['A. School Bus Driver',
+                 'B. Waiter',
+                 'C. Cop',
+                 'D. Bartender'],
+    flags : [true, false, false, false],
+    answer : 'A. School Bus Driver'
+};
 
-     }; 
+var q7 = {
+    question : 'In "St. Elmo\'s Fire," What city does Billy go to at the end?',
+    possibleAnswers : ['A. San Fransisco',
+                 'B. Chicago',
+                 'C. New York',
+                 'D. Washington DC'],
+    flags : [false, false, true, false],
+    answer : 'C. New York'
+};
 
-     var correct = 0; 
-     var wrong = 0; 
-     var q1 = {
+var q8 = {
+    question : 'What is the name of the evil spirit in Polterguise?',
+    possibleAnswers : ['A. Lucifer',
+                 'B. Kane',
+                 'C. Death',
+                 'D. None of the above'],
+    flags : [false, true, false, false],
+    answer : 'B. Kane'
+};
 
-     	question: "What year was Fast Times at Ridgemont High released?"
-     	possibleAnswer : ["A. 1983", 
-     						    "B. 1988",
-     							"C. 1988",
-     							"D. 1982"],
-        flags : [false, false, false, true],
-        answer : "D. 1982"
-     }; 
+var q9 = {
+    question : 'In the movie "Sixteen Candles," what was Sam\'s grandparents exchange students name',
+    possibleAnswers : ['A. Bruce Lee',
+                 'B. Lucky Lee Phong',
+                 'C. Data',
+                 'D. Lucky Duck Dong'],
+    flags : [false, false, false, true],
+    answer : 'D. Lucky Duck Dong'
+};
 
-    var q2 = {
-    	question: "What was Chunk's real name in The Goonies?"
-    	possibleAnswer : ["A. Laurence Chunk Cohen", 
-     						    "B. James Chunk Roberts",
-     							"C. Tommy Chunk Blanks",
-     							"D. Data Chunk Li"],
-        flags : [true, false, false, false],
-        answer : "A. Laurence Chunk Cohen"
-    }; 
+var q10 = {
+    question : 'In the 1983 movie "National Lampoon\'s Vacation," where were the Griswolds headed on their cross country trip?',
+    possibleAnswers : ['A. Disney World',
+                  'B. Walley World',
+                  'C. Gator World',
+                  'D. Sea World'],
+    flags : [false, true, false, false],
+    answer : 'B. Walley World'
+}
 
-    var q3 = {
-    	question: "What is the name of the airline that operates the ill-fated flight from LA to Chicago in the movie Airplane?"
-    	possibleAnswer : ["A. Pan Am Airline", 
-     						    "B. American Wings",
-     							"C. Trans American",
-     							"D. Intercontinental Express"],
-        flags : [false, false, true, false],
-        answer : "C. Trans American"
-    }; 
+var questionArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 
-    var q4 = {
-    	question: "In the movie The Goonies what brand of candybar did Chunk try to give to Sloth?"
-    	possibleAnswer : ["A. Butter Finger", 
-     						    "B. Nestle Crunch",
-     							"C. Baby Ruth",
-     							"D. Milky Way"],
-        flags : [false, false, true, false],
-        answer : "C. Baby Ruth"
-    }; 
+function loadQuestion(questionSelection) {
+    console.log(questionSelection);
+    countdownTimer.reset();
+  $(".question").html("<h3>" + questionArray[questionSelection].question + "</h3>");
+  $("#buttonA").text(questionArray[questionSelection].possibleAnswers[0]).show();
+  $("#buttonB").text(questionArray[questionSelection].possibleAnswers[1]).show();
+  $("#buttonC").text(questionArray[questionSelection].possibleAnswers[2]).show();
+  $("#buttonD").text(questionArray[questionSelection].possibleAnswers[3]).show();
 
-    var q5 = {
-    	question: "In The Karate Kid what color did Daniel have to paint Miagi's house, as part of his training."
-    	possibleAnswer : ["A. Brown", 
-     						    "B. Red",
-     							"C. Blue",
-     							"D. Green"],
-        flags : [false, false, false, true],
-        answer : "D. Green"
-    }; 
-
-    var q6 = {
-    	question: "What does Ally Sheedy say she likes to drink in the Breakfast Club?"
-    	possibleAnswer : ["A. Iced Tea", 
-     						    "B. Milk",
-     							"C. Vodka",
-     							"D. Mimosa"],
-        flags : [false, false, true, false],
-        answer : "C. Vodka"
-    }; 
-
-    var q7 = {
-    	question: "What was the license number on the Ghostbusters' car?" 
-    	possibleAnswer : ["A. Booo-1", 
-     						    "B. ECTO-1",
-     							"C. SCRD-1",
-     							"D. ED3C4"],
-        flags : [false, true, false, false],
-        answer : "B. ECTO-1"
-    }; 
-
-    var q8 = {
-    	question: "In Grease Two what does stephanie want in a boyfriend?"
-    	possibleAnswer : ["A. Hard Worker", 
-     						    "B. Nerd",
-     							"C. A Cool Rider",
-     							"D. Just to be single"],
-        flags : [false, false, true, false],
-        answer : "C. A Cool Rider"
-    }; 
-
-    var q9 = {
-    	question: "In National Lampoon's Vacation, what did the Griswalds call their ugly green station wagon?"
-    	possibleAnswer : ["A. The Family Truckster", 
-     						    "B. The Giswaldster",
-     							"C. The Griswagon",
-     							"D. The Wagster"],
-        flags : [true, false, false, false],
-        answer : "A. The Family Truckster"
-    }; 
-
-    var q10 = {
-    	question: "What fraternity do the Lewis and Gilbert join in Revenge of the Nerds?"
-    	possibleAnswer : ["A. Alpha Beta Capa", 
-     						    "B. Alpha Alpha Alpha",
-     							"C. Lambda Lambda Lambda",
-     							"D. The Royal Alpha Males"],
-        flags : [false, false, true, false],
-        answer : "C. Lambda Lambda Lamda"
-    }; 
-
-   var questionArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]; 
-   
+}
 
 
 
+function setup() {
+    index = 0;
+    $('.question').append('<button id="startButton">Start</button>');
+    $('#startButton').on('click', function() {
+        $(this).hide();
+        countdownTimer.start();
+        loadQuestion(index);
+    });
+}       
+
+function getAnswer() {
 
 
+    $('.answerchoice').on('click', function() {
+      console.log('alert', index);
+        index++;
+        console.log('click', index);
+        $(".question").text('');
+        $("#buttonA").text('');
+        $("#buttonB").text('');
+        $("#buttonC").text('');
+        $("#buttonD").text('');
+        loadQuestion();
+    })
+}
+
+function answerCorrect() {
+    correct++;
+    alert("Correct!");
+    console.log("correct");
+}
+
+function answerWrong() {
+    wrong++;
+    alert("Incorrect!");
+    console.log("wrong");
+}
+
+function showScore() {
+    $('.question').empty();
+    $('.question').append("<h2><p>" + correct + " correct</p></h2>");
+    $('.question').append("<h2><p>" + wrong + " incorrect</p></h2>");
+    countdownTimer.stop();
+    $('.timer').empty();
+
+}
+
+setup();
+$('.answerchoice').on('click', function() {
+ console.log($(this));
+ if(this.id == 'buttonA') {
+    var answerChosen = 'A';
+ } else if(this.id == 'buttonB') {
+    answerChosen = 'B';
+ } else if (this.id == 'buttonC') {
+    answerChosen = 'C';
+ } else if (this.id == 'buttonD') {
+    answerChosen = 'D';
+ } 
+ if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
+    answerCorrect();
+ } else if (answerChosen == 'A') {
+    answerWrong();
+ }
+ if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
+    answerCorrect();
+ } else if (answerChosen == 'B') {
+    answerWrong();
+ }
+if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
+    answerCorrect();
+ } else if (answerChosen == 'C') {
+    answerWrong();
+ }
+if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
+    answerCorrect();
+ } else if (answerChosen == 'D') {
+    answerWrong();
+ }
+
+ $(".question").text('');
+ $("#buttonA").text('');
+ $("#buttonB").text('');
+ $("#buttonC").text('');
+ $("#buttonD").text('');
+ index++;
+ if (index < questionArray.length) {
+    loadQuestion(index);
+ } else {
+    $(".answerchoice").hide();
+    showScore();
+ }
+});
 
 
-
-
-
-
-
-
-
-
-
-
+//  $('#start').click(countdownTimer.start);
+});
